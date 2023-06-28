18d5dc3821bca806a4ebe9a87df8a710bab246fa
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-28 14:14:46 +0530

    Add project files.

---
+++ Abstract/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(10);
+            c1.CalculateArea();
+            Console.WriteLine(c1);
+
+            Rectangle r = new Rectangle(5, 14);
+            r.CalculateArea();
+            Console.WriteLine(r);
+        }
+    }
+}
